1c41793477d60dd38512e46313e7a5e350b21190
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2017-10-10 17:41:54 +0200

    Added nuget packages for FluentAssertions and NUnitTestAdapter.

--- Crossword-generator/Tests/CrosswordManagerTests.cs
+++ Crossword-generator/Tests/CrosswordManagerTests.cs
@@ -1,9 +1,15 @@
 ﻿using NUnit.Framework;
+using FluentAssertions;
 
 namespace Tests
 {
     [TestFixture]
     public class CrosswordManagerTests
     {
+        [Test]
+        public void load_words_from_non_existing_file_should_throw_an_exception()
+        {
+            //TODO: Test
+        }
     }
 }
