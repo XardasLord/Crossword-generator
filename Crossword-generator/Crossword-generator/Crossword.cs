95616031ac2ac6737b71c7df27afb1e22e151b2d
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2017-11-30 19:51:12 +0100

    Automatically focusing next cell

--- Crossword-generator/Crossword-generator/Crossword.cs
+++ Crossword-generator/Crossword-generator/Crossword.cs
@@ -44,9 +44,7 @@ namespace Crossword_generator
 
         private void dgvBoard_EditingControlShowing(object sender, DataGridViewEditingControlShowingEventArgs e)
         {
-            DataGridViewTextBoxEditingControl tb = (DataGridViewTextBoxEditingControl)e.Control;
-            tb.KeyPress += new KeyPressEventHandler(dgvBoard_KeyPress);
-
+            e.Control.KeyPress -= dgvBoard_KeyPress;
             e.Control.KeyPress += new KeyPressEventHandler(dgvBoard_KeyPress);
         }
 
@@ -55,9 +53,11 @@ namespace Crossword_generator
             if (dgvBoard.CurrentCell == null || dgvBoard.CurrentCell.Tag == null || dgvBoard.CurrentCell.Tag.ToString() == string.Empty)
                 return;
 
-            dgvBoard.CurrentCell.Value = e.KeyChar;
+            dgvBoard.CurrentCell.Value = e.KeyChar.ToString().ToUpper();
 
             FocusNextCell();
+
+            e.Handled = true;
         }
 
         private void DrawBoard()
@@ -108,11 +108,15 @@ namespace Crossword_generator
             dgvBoard[column, row].Style.Font = new Font("Arial", 20F, GraphicsUnit.Pixel);
             dgvBoard[column, row].ReadOnly = false;
 
-            if(CheckIfPasswordField(column, row))
+            //TODO: Tooltip for each element
+            //var crosswordElement = _board.Elements.Select(x => x.Word).Where(a => a.).FirstOrDefault();
+            //dgvBoard[column, row].ToolTipText = crosswordElement.Word.Description;
+
+            if(IsPasswordField(column, row))
                 dgvBoard[column, row].Style.BackColor = Color.Yellow;
         }
 
-        private bool CheckIfPasswordField(int column, int row)
+        private bool IsPasswordField(int column, int row)
         {
             var isPasswordField = false;
 
@@ -129,12 +133,52 @@ namespace Crossword_generator
             return isPasswordField;
         }
 
+        private bool IsLetterField(int column, int row)
+        {
+            var isLetterField = false;
+
+            if (dgvBoard[column, row].Tag != null && !string.IsNullOrEmpty(dgvBoard[column, row].Tag.ToString()))
+                isLetterField = true;
+
+            return isLetterField;
+        }
+
         private void FocusNextCell()
         {
             var column = dgvBoard.CurrentCell.ColumnIndex;
             var row = dgvBoard.CurrentCell.RowIndex;
 
+            if(_board.Columns - 1 > column)
+            {
+                column += 1;
 
+                if(IsLetterField(column, row))
+                {
+                    dgvBoard.CurrentCell = dgvBoard.Rows[row].Cells[column];
+                    return;
+                }
+
+            }
+
+            if(_board.Rows - 1 > row)
+            {
+                row += 1;
+
+                if (_board.Elements.Length == row)
+                {
+                    dgvBoard.CurrentCell = dgvBoard.Rows[0].Cells[0];
+                    return;
+                }
+
+                var nextRowCoordinates = _board.Elements.SelectMany(x => x.CoordinatesInfo).Where(x => x.Coordinates.X == row).ToArray();
+                column = nextRowCoordinates[0].Coordinates.Y;
+
+                if (IsLetterField(column, row))
+                {
+                    dgvBoard.CurrentCell = dgvBoard.Rows[row].Cells[column];
+                    return;
+                }
+            }
         }
     }
 }
